391842efb0488c9cafd5e0e6c2bdf0d1a8a20847
somnak Kalan <kalansomnak12@gmail.com>
2022-09-09 08:04:05 +0700

    ListCard

---
+++ front/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        <div class="card-header">Somnak Kalan</div>
+        <div class="card-body">Hello this  body</div>
+        <div class="card-footer">This footer</div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
